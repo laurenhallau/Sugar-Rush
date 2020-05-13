62aef0b12b40c35148a76791c7e650e3e6f1ba93
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-13 09:22:19 -0500

    dependencies added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Jumbotron from "./Components/Jumbotron/Jumbotron.js"
+import Header from "./Components/Header/Header.js"
+import Footer from "./Components/Footer/Footer.js"
+// import { BrowserRouter as Switch, Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Jumbotron />
+      <Footer />
+   
     </div>
   );
 }
