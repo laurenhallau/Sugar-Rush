04111b901eeebe50cb8eb4c456608cc527696a39
Steve Kindt <stevemkindt@gmail.com>
2020-05-13 19:25:41 -0500

    main content and search area

---
+++ client/src/SearchArea.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchArea = (props) => {
+  return (
+    <div className="search-area">
+      <form action="">
+        <input onChange={props.handleSearch} type="text" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchArea;
