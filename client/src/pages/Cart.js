8fff4a4ab743d0cec4885a8c5ff417e8dfe292d1
Kelly Bazella <kelly.bazella@gmail.com>
2020-05-14 21:37:52 -0500

    added pages functions

---
+++ client/src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+function Cart(){
+    //functions
+
+    return(
+        <h1>Cart</h1>
+    )
+};
+
+export default Cart;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+function Home(){
+    //functions
+
+    return(
+        <h1>Home Page</h1>
+    )
+};
+
+export default Home;
