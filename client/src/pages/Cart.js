0c9191d925f7058143a8bdbd09321052cacaf4f6
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-15 11:27:23 -0500

    added shopping cart page info. next will work on getting info from db

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react';
+import React from "react";
+import { Button, Table, Container } from "react-materialize";
+import { Cart as ShoppingCart } from "../Components/CartTable/Cart.js";
 
-function Cart(){
-    //functions
-
-    return(
-        <h1>Cart</h1>
-    )
-};
+function Cart() {
+  return (
+    <div className="cart-container">
+      <Container>
+        <h4 style={{ textAlign: "left" }}>Shopping Cart</h4>
+        <h6 style={{ textAlign: "left" }}>(Restaurant Name)</h6>
+        <Table>
+          <thead>
+            <tr>
+              <th data-field="name">Item Name</th>
+              <th data-field="name">Quanitity</th>
+              <th data-field="price">Item Price</th>
+            </tr>
+          </thead>
+          <ShoppingCart />
+          <Button style={{ marginBottom: "2rem" }}>Checkout</Button>
+        </Table>
+      </Container>
+    </div>
+  );
+}
 
 export default Cart;
