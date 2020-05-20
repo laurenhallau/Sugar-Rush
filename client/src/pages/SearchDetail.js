1f47fe7fdee61e186f9bc99c9585f1db79cad01e
Kelly Bazella <kelly.bazella@gmail.com>
2020-05-19 21:44:00 -0500

    work on search details page

--- client/src/pages/SearchDetail.js
+++ client/src/pages/SearchDetail.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { Component } from "react";
+import API from "../utils/API";
 
-function SearchDetail(){
-    //functions
+class SearchDetail extends Component {
+  //functions
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: [],
+    };
+  }
 
-    return(
-        <h1>SearchDetail</h1>
-    )
-};
+  componentDidMount = () => {
+    API.getRestaurants().then((data) => {
+      this.setState({
+        restaurants: data.data,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="searchDetail">
+        {this.state.restaurants.map((item) => {
+          return (
+            <div>
+              <h1>{item.name}</h1>
+              <h3>Address: {item.location.address}</h3>
+              <h4>City: {item.location.locality}</h4>
+            
+
+            </div>
+          );
+        })}
+                      <div className="row">
+              <div className="col s12">
+                <ul className="tabs z-depth-1">
+                  <li className="tab col s3"><a href="#test1">Test 1</a></li>
+                  <li className="tab col s3"><a className="active" href="#test2">Test 2</a></li>
+                  <li className="tab col s3"><a href="#test3">Test 4</a></li>
+                  <li className="tab col s3"><a href="#test4">Test 4</a></li>
+                </ul>
+              </div>
+              <div id="test1" className="col s12">Test 1</div>
+              <div id="test2" className="col s12">Test 2</div>
+              <div id="test3" className="col s12">Test 3</div>
+              <div id="test4" className="col s12">Test 4</div>
+            </div>
+      </div>
+    );
+  }
+}
 
 export default SearchDetail;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,7 @@ const BASEURL = "https://developers.zomato.com/api/v2.1/search?q=";
 const APIKEY = "3ee3d740dc7385dd0034e55132458a30";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
+
 export default {
   search: function (query) {
     return axios.get(BASEURL + query + APIKEY);
