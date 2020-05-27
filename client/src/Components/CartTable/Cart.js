ff93fc2fa5000fd0143e9a9d37bdb92fafbc9cfc
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-26 22:34:49 -0500

    increment, decrement, and remove from cart functions

--- client/src/Components/CartTable/Cart.js
+++ client/src/Components/CartTable/Cart.js
@@ -1,53 +1,95 @@
 import React, { Component } from "react";
 import uuid from "uuid";
+import API from "../../utils/API";
 
 export class Cart extends Component {
-  state = {
-    items: [
-      { id: uuid(), item: { name: "Cookie", quantity: 12, price: 10.0 } },
-      { id: uuid(), item: { name: "Pie", quantity: 12, price: 10.0 } },
-      { id: uuid(), item: { name: "Ice Cream", quantity: 12, price: 10.0 } },
-      { id: uuid(), item: { name: "Cupcake", quantity: 12, price: 10.0 } },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: [],
+    };
+  }
+  componentWillMount() {
+    API.renderCart()
+      .then((response) => {
+        this.setState({ cart: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // items: [
+    //   { id: uuid(), item: { name: "Cookie", quantity: 12, price: 10.0 } },
+    //   { id: uuid(), item: { name: "Pie", quantity: 12, price: 10.0 } },
+    //   { id: uuid(), item: { name: "Ice Cream", quantity: 12, price: 10.0 } },
+    //   { id: uuid(), item: { name: "Cupcake", quantity: 12, price: 10.0 } },
+    // ],
+  }
 
   render() {
-    const increment = () => {
-      console.log("Add button clicked");
+    console.log(this.state);
+    const changeQuantity = (id, amount) => {
+      console.log("add button clicked!");
+      console.log("id: " + id);
+      let newItem = this.state.cart.find((cart) => cart._id === id);
+      newItem.quantity = newItem.quantity + amount;
+      this.setState((prevState) => ({
+        cart: prevState.cart.map((item) => {
+          if (item._id === id) {
+            return newItem;
+          }
+          return item;
+        }),
+      }));
+    };
+    const increment = (id) => {
+      changeQuantity(id, 1)
     };
 
-    const decrement = () => {
+    const decrement = (id) => {
       console.log("Subtract button clicked");
+      changeQuantity(id, -1)
     };
 
-    const clear = () => {
+    const clear = (id) => {
       console.log("Clear button clicked");
+      const newCart = this.state.cart.filter(c => c._id !== id)
+      this.setState({cart: newCart})
+      const cartToRemove = this.state.cart.find(c => c._id === id)
+      API.removeFromCart(cartToRemove)
+      
     };
 
-    const { items } = this.state;
-    return items.map(({ item }) => {
-      return (
-        <tbody>
-          <tr>
-            <td>{item.name}</td>
-            <td>
-              <i className="tiny material-icons" style={{ margin: "0px 5px" }} onClick={decrement}>
-                remove_circle_outline
-              </i>
-              {item.quantity}
-              <i className="tiny material-icons" style={{ margin: "0px 5px" }} onClick={increment}>
-                add_circle_outline
-              </i>
-            </td>
-            <td>{item.price}</td>
-            <td>
-              <i className="tiny material-icons" onClick={clear}>
-                clear
-              </i>
-            </td>
-          </tr>
-        </tbody>
-      );
-    });
+    const { cart } = this.state;
+    return cart.map((item) => (
+      <tbody>
+        <tr>
+          <td>{item.description}</td>
+          <td>
+            <i
+              className="tiny material-icons"
+              style={{ margin: "0px 5px" }}
+              onClick={() => decrement(item._id)}
+            >
+              remove_circle_outline
+            </i>
+            {item.quantity}
+            <i
+              className="tiny material-icons"
+              style={{ margin: "0px 5px" }}
+              onClick={() => increment(item._id)}
+            >
+              add_circle_outline
+            </i>
+          </td>
+          <td>{item.price}</td>
+          <td>
+            <i className="tiny material-icons" onClick={() => clear(item._id)}>
+              clear
+            </i>
+          </td>
+        </tr>
+      </tbody>
+    ));
   }
 }
